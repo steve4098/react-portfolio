1ab1f551fc46e5317c18a13e4d3c3dc530dac960
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-10-20 19:50:14 +0100

    jumbotron component populated with bootstrap code

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function jumbotron (props) {
+    return(
+        <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1>Bootstrap Tutorial</h1>      
+    <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile-first projects on the web.</p>
+  </div>
+</div>
+    )
+}
+
+export default jumbotron;
