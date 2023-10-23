a3e521d377050faa60a1f1222ae8d4956d4f5fc6
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-10-23 10:43:46 +0100

    bootstrap code for two columns added to about me component

---
+++ src/components/AboutMe.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function aboutMe (props) {
+    return(
+        <div class="row justify-content-around">
+        <div class="col-5">
+          One of two columns
+        </div>
+        <div class="col-5">
+          One of two columns
+        </div>
+      </div>
+    )
+}
+
+export default aboutMe;
