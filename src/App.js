01b897163a89856fd2c26b8226b711f630f2c3e8
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-10-23 13:41:28 +0100

    import bootsrap added to index.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+// import bootstrap;
 import NavbarBts from "./components/NavbarBts";
 import Jumbotron from "./components/Jumbotron";
 import AboutMe from "./components/AboutMe"
