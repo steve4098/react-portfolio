cbfd90592c286b3904c2bda3d7d2f8b9a8149861
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-10-27 18:22:05 +0100

    image added to images folder for jumbotron background. import link for css file updated. css file updated with code for jumbotron

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.css';
-import css from './index.css';
+import './css/index.css';
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
