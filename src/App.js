da3fbb7f454c129a59a23b6e83e75f41ffb8a18f
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-10-27 20:03:53 +0100

    import and funcrion added to home.jsx NAvbar, jumbotron and about me component calls pulled from app.js and replaced with home

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
-import NavbarBts from "./components/NavbarBts";
-import Jumbotron from "./components/Jumbotron";
-import AboutMe from "./components/AboutMe"
+import Home from "./pages/Home";
 
 
 function App() {
   return (
-    <>
-      <NavbarBts />,
-      <Jumbotron />,
-      <AboutMe />
-    </>
+  
+    <Home />
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavbarBts from "../components/NavbarBts";
+import Jumbotron from "../components/Jumbotron";
+
+function Home () {
+    return (
+        <>
+        <NavbarBts />,
+        <Jumbotron />
+        </>
+    )
+}
+
+export default Home;
