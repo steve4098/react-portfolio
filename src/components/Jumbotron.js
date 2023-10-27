df334fe3011b45b49dd1cf64c651f1910ce74142
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-10-27 18:30:21 +0100

    classes renamed to link jumbotron.js to index.css

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,13 +1,14 @@
 import React from "react";
+import '../css/index.css';
 
 function jumbotron (props) {
     return(
-        <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1>Steve Butler</h1>      
-    <p>This portfolio (made using React) shows some of the projects I have created in my coding career to date</p>
-  </div>
-</div>
+        <div class="jumbotron jumbotron-fluid jumboBackground">
+          <div class="container jumboCopy">
+           <h1>Steve Butler</h1>      
+           <p>This portfolio (made using React) shows some of the projects I have created in my coding career to date</p>
+          </div>
+        </div>
     )
 }
 
