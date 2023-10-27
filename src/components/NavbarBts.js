12d1e36e9e5731f6dbf76b339cd1f0560f70e799
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-10-27 19:15:38 +0100

    navbarbts.js updated to change <a href> to <link to>

--- src/components/NavbarBts.js
+++ src/components/NavbarBts.js
@@ -1,26 +1,27 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 function navbar() {
     return (
         <div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+  
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <Link to = "/Home">Home</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Projects</a>
+          <Link to = "/Projects">Projects</Link>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li class="nav-item">
+          <Link to = "/ContactMe">
             Contact Me
-          </a>
+          </Link>
         </li>
       </ul>
     </div>
